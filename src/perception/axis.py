dde7305f518db43c7d4f5fb2da281273de5c455b
Kameron Dawson <ksdawson@mit.edu>
2025-11-20 19:17:04 -0500

    More work on main axis

--- src/perception/axis.py
+++ src/perception/axis.py
@@ -1,5 +1,9 @@
 import numpy as np
-from sklearn.cluster import DBSCAN
+
+######################################################################
+# Compute main axis of piece cloud with vector pointing in the
+# direction of the top of the piece
+######################################################################
 
 def get_cloud_main_axis_pca(point_cloud):
     pts = point_cloud.xyzs().T # Nx3
@@ -18,7 +22,6 @@ def get_cloud_main_axis_pca(point_cloud):
     main_axis = eigenvectors[:, idx]
 
     return main_axis
-
 
 def get_piece_cloud_main_axis(piece_cloud):
     # Get main axis
@@ -47,78 +50,3 @@ def get_piece_cloud_main_axis(piece_cloud):
             # Flip
             main_axis = -main_axis
     return main_axis
-
-
-######################################################################
-# Functions to determine direction of main axis using circle fitting on the perp plane
-######################################################################
-
-def project_cloud_onto_main_axis_perp_plane(point_cloud, main_axis):
-    pts = point_cloud.xyzs().T # Nx3
-
-    # Center the cloud
-    centroid = np.mean(pts, axis=0)
-    pts_centered = pts - centroid
-
-    # Compute two perpendicular vectors to define the plane
-    # Pick an arbitrary vector not parallel to main_axis
-    arbitrary = np.array([1, 0, 0]) if abs(main_axis[0]) < 0.9 else np.array([0, 1, 0])
-    u = np.cross(main_axis, arbitrary)
-    u /= np.linalg.norm(u)
-    v = np.cross(main_axis, u)
-    v /= np.linalg.norm(v)
-
-    # Project points onto the plane
-    proj_coords_u = np.dot(pts_centered, u)
-    proj_coords_v = np.dot(pts_centered, v)
-    heights = np.dot(pts_centered, main_axis) # height along main_axis
-
-    return proj_coords_u, proj_coords_v, heights
-
-def fit_circle_2d(x, y):
-    A = np.c_[2*x, 2*y, np.ones_like(x)]
-    b = x**2 + y**2
-    c, residuals, rank, s = np.linalg.lstsq(A, b, rcond=None)
-    x0, y0, c0 = c
-    r = np.sqrt(c0 + x0**2 + y0**2)
-    # compute mean squared error
-    dists = np.sqrt((x - x0)**2 + (y - y0)**2)
-    mse = np.mean((dists - r)**2)
-    return np.array([x0, y0]), r, mse
-
-def get_cloud_main_axis(point_cloud):
-    # Get cloud main axis
-    main_axis = get_cloud_main_axis_pca(point_cloud)
-
-    # Project cloud onto the plane perpendicular to the main axis
-    x, y, z = project_cloud_onto_main_axis_perp_plane(point_cloud, main_axis)
-
-    # DBSCAN params
-    z_range = np.max(z) - np.min(z)
-    eps = z_range / 100
-    min_samples = len(z) / 100
-    z_reshaped = z.reshape(-1, 1) # needed since z is a 1D array
-
-    # Cluster the z values to define slices along the main axis
-    db = DBSCAN(eps=eps, min_samples=min_samples).fit(z_reshaped)
-    labels = db.labels_
-    unique_labels = [l for l in np.unique(labels) if l != -1] # skip noise
-    
-    # Group x and y by cluster
-    clusters = {}
-    for label in unique_labels:
-        mask = labels == label
-        cluster_x = x[mask]
-        cluster_y = y[mask]
-        cluster_z = z[mask]
-        clusters[label] = {
-            'x': cluster_x,
-            'y': cluster_y,
-            'z': cluster_z
-        }
-
-    # Fit a circle to each cluster
-    for cluster in clusters:
-        x, y = clusters[cluster]['x'], clusters[cluster]['y']
-        x0, y0, r, mse = fit_circle_2d(x, y)
-        print(x0, y0, r, mse)
